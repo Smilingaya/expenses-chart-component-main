44bcd13f36aba49dcd446e654ae974e276ee479a
aya <alizeroukiaya0911@gmail.com>
2023-08-07 00:41:36 +0200

    project

---
+++ app.js
@@ -0,0 +1,76 @@
+let b1=document.querySelector('.b1');
+let prime1=document.querySelector('.prime1');
+const b2=document.querySelector('.b2');
+const prime2=document.querySelector('.prime2');
+const b3=document.querySelector('.b3');
+const prime3=document.querySelector('.prime3');
+const b4=document.querySelector('.b4');
+const prime4=document.querySelector('.prime4');
+const b5=document.querySelector('.b5');
+const prime5=document.querySelector('.prime5');
+const b6=document.querySelector('.b6');
+const prime6=document.querySelector('.prime6');
+const b7=document.querySelector('.b7');
+const prime7=document.querySelector('.prime7');
+function show1(){
+    prime1.style.display="block";
+}
+b1.addEventListener('mouseover',show1);
+function none1(){
+    prime1.style.display="none";
+}
+b1.addEventListener('mouseout',none1);
+function show2(){
+    prime2.style.display="block";
+}
+b2.addEventListener('mouseover',show2);
+function none2(){
+    prime2.style.display="none";
+}
+b2.addEventListener('mouseout',none2);
+function show3(){
+    prime3.style.display="block";
+}
+b3.addEventListener('mouseover',show3);
+function none3(){
+    prime3.style.display="none";
+}
+
+b3.addEventListener('mouseout',none3);
+function show4(){
+    prime4.style.display="block";
+}
+b4.addEventListener('mouseover',show4);
+
+function none4(){
+    prime4.style.display="none";
+}
+b4.addEventListener('mouseout',none4);
+function show5(){
+    prime5.style.display="block";
+}
+
+function none5(){
+    prime5.style.display="none";
+}
+b5.addEventListener('mouseout',none5);
+b5.addEventListener('mouseover',show5);
+function none6(){
+    prime6.style.display="none";
+}
+b6.addEventListener('mouseout',none6);
+function show6(){
+    prime6.style.display="block";
+}
+b6.addEventListener('mouseover',show6);
+function show7(){
+    prime7.style.display="block";
+}
+function none7(){
+    prime7.style.display="none";
+}
+b7.addEventListener('mouseout',none7);
+b7.addEventListener('mouseover',show7);
+
+
+
